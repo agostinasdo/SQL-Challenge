9ebdfb036cf1a9a99361d3cbdcffb0ef28ed6846
agostinasdo <103266868+agostinasdo@users.noreply.github.com>
2025-06-09 14:19:32 -0300

    Update ejercicio_4.sql

--- ejercicio_4.sql
+++ ejercicio_4.sql
@@ -1,5 +1,11 @@
 -- EJERCICIO 4: Tiempo de entrega promedio (en min) en marzo de 2019, por país.
 
+-- CTE que calcula la duración de entrega en minutos para cada orden.
+-- Se utiliza EXTRACT(EPOCH) para obtener la diferencia entre fechas en segundos, luego se divide por 60 para convertirlo a minutos.
+
+-- NOTA: Se reemplaza manualmente '2019-02-29' por '2019-02-28' para evitar errores de conversión,
+-- ya que 2019 no fue un año bisiesto y algunas fechas están mal formateadas en la fuente de datos.
+
 WITH orders_with_delivery_time AS (
   SELECT *,
    EXTRACT(EPOCH FROM (
@@ -8,10 +14,12 @@ WITH orders_with_delivery_time AS (
   FROM orders_sampleorders_sample
 )
 
+-- Se agrupan los resultados por país y se calcula el promedio del tiempo de entrega en minutos.
+-- Solo se incluyen órdenes con fecha de compra en marzo 2019.
 SELECT 
   cc.country_name AS country,
-  AVG (delivery_minutes) AS avg_delivery_time
+  ROUND(AVG (delivery_minutes),2) AS avg_delivery_time
 FROM orders_with_delivery_time od
-JOIN city_country_mapcitiessample cc ON od.city_id = cc.city_id
+INNER JOIN city_country_mapcitiessample cc ON od.city_id = cc.city_id
 WHERE TO_CHAR(od.order_date::timestamp, 'YYYY-MM') = '2019-03'
 GROUP BY country;
