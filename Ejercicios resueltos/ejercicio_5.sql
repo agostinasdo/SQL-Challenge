e9614db7f8e45715795d21f98aedf81c9cabd06e
agostinasdo <103266868+agostinasdo@users.noreply.github.com>
2025-06-09 17:04:25 -0300

    Update ejercicio_5.sql

--- Ejercicios resueltos/ejercicio_5.sql
+++ Ejercicios resueltos/ejercicio_5.sql
@@ -1,14 +1,38 @@
 -- EJERCICIO: Para los 50 customers con más compras de la base, calcular qué porcentaje de estas compras se realizó
 -- en los top 50 sellers. El top de sellers debe calcularse por monto, y el top de customers no debe calcularse por país.
 
--- Calcula el top 50 de sellers según el monto total vendido (en moneda local)
-WITH top_sellers AS (
+-- Se utiliza USD para poder comparar montos entre países de forma consistente.
+-- Se limpian los datos de tipo de cambio y se ajusta la posición del punto decimal según la moneda
+WITH currency_cleaned AS (
+  SELECT *, REPLACE(rate_us, '.', '') AS rate_us_clean
+  FROM currency_mapcurrency
+),
+currency_prepared AS (
+  SELECT *,
+    CAST(
+      CASE
+        WHEN currency_iso IN ('ARS','UYU') THEN
+          LEFT(rate_us_clean, 2) || '.' || RIGHT(rate_us_clean, LENGTH(rate_us_clean) - 2)
+        WHEN currency_iso = 'CLP' THEN
+          LEFT(rate_us_clean, 3) || '.' || RIGHT(rate_us_clean, LENGTH(rate_us_clean) - 3)
+        WHEN currency_iso IN ('COP','PYG') THEN
+          LEFT(rate_us_clean, 4) || '.' || RIGHT(rate_us_clean, LENGTH(rate_us_clean) - 4)
+      END AS DECIMAL(20,8)
+    ) AS exchange_rate_usd
+  FROM currency_cleaned
+),
+	
+-- Calcula el top 50 de sellers según el monto total vendido (en USD)
+top_sellers AS (
   SELECT seller_id,
-         SUM(order_amount_local_currency) AS total_amount
-  FROM orders_sampleorders_sample
-  WHERE order_status = 'CONFIRMED'
+         SUM(order_amount_local_currency / exchange_rate_usd) AS total_amount_usd
+  FROM orders_sampleorders_sample o
+  INNER JOIN city_country_mapcitiessample cc
+  	ON o.city_id = cc.city_id
+  INNER JOIN currency_prepared cp
+  	ON cc.country_id = cp.country_id
   GROUP BY seller_id
-  ORDER BY total_amount DESC
+  ORDER BY total_amount_usd DESC
   LIMIT 50
 ),
 
@@ -25,10 +49,10 @@ top_customers AS (
 
 -- Trae el detalle de todas las órdenes confirmadas que pertenecen a los 50 clientes principales.
 orders_top_customers AS (
-  SELECT o.*
-  FROM orders_sampleorders_sample o
-  INNER JOIN top_customers tc ON o.customer_id = tc.customer_id
-  WHERE o.order_status = 'CONFIRMED'
+  SELECT o2.*
+  FROM orders_sampleorders_sample o2
+  INNER JOIN top_customers tc ON o2.customer_id = tc.customer_id
+  WHERE o2.order_status = 'CONFIRMED'
 )
 
 -- Se calula: la cantidad total de órdenes confirmadas realizadas por ese customer y
