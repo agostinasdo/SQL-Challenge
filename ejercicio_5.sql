4501be1b4c10fa6a49022e8e0bf08560b7818873
agostinasdo <103266868+agostinasdo@users.noreply.github.com>
2025-06-09 14:25:17 -0300

    Update ejercicio_5.sql

--- ejercicio_5.sql
+++ ejercicio_5.sql
@@ -1,3 +1,7 @@
+-- EJERCICIO: Para los 50 customers con más compras de la base, calcular qué porcentaje de estas compras se realizó
+-- en los top 50 sellers. El top de sellers debe calcularse por monto, y el top de customers no debe calcularse por país.
+
+-- Calcula el top 50 de sellers según el monto total vendido (en moneda local)
 WITH top_sellers AS (
   SELECT seller_id,
          SUM(order_amount_local_currency) AS total_amount
@@ -8,6 +12,7 @@ WITH top_sellers AS (
   LIMIT 50
 ),
 
+-- Calcula el top 50 de customers según la cantidad de órdenes confirmadas.
 top_customers AS (
   SELECT customer_id,
          COUNT(*) AS cantidad_compras
@@ -18,6 +23,7 @@ top_customers AS (
   LIMIT 50
 ),
 
+-- Trae el detalle de todas las órdenes confirmadas que pertenecen a los 50 clientes principales.
 orders_top_customers AS (
   SELECT o.*
   FROM orders_sampleorders_sample o
@@ -25,6 +31,8 @@ orders_top_customers AS (
   WHERE o.order_status = 'CONFIRMED'
 )
 
+-- Se calula: la cantidad total de órdenes confirmadas realizadas por ese customer y
+-- el porcentaje de esas órdenes que fueron hechas a sellers que están en el top 50 de ventas.
 SELECT 
   otc.customer_id,
   COUNT(*) AS qty_confirmed_orders,
